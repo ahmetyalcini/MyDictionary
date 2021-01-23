88e422b33d0059599ce665cbb7329576e35e2934
Ahmet Yalçın <ahmetyalcini@users.noreply.github.com>
2021-01-23 20:59:29 +0300

    Add project files.

---
+++ Lists/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> dictonary = new MyDictionary<string, int>();
+
+            dictonary.Add("Ahmet", 1);
+            dictonary.Add("Selin", 2);
+            dictonary.Add("Oğuz", 3);
+            Console.WriteLine(dictonary.Count);
+            Console.ReadKey();
+        }
+    }
+}
